6a47061b9a5eefbbb773a8182cfbd3f943d35caf
KuanyshR <kuanysh.rsymbetov@gmail.com>
2024-11-23 01:46:22 +0500

    initial commit

---
+++ src/main/java/com/epam/learn/ai/AiMoviePromptServiceApplication.java
@@ -0,0 +1,13 @@
+package com.epam.learn.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiMoviePromptServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiMoviePromptServiceApplication.class, args);
+	}
+
+}
